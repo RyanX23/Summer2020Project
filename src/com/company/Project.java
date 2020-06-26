a8c3c72d567931f6c183c8e993deef6196518f57
Ryan Xie <xier23@greenhill.org>
2020-06-26 09:35:00 -0500

    Final Project and Presentation

--- src/com/company/Project.java
+++ src/com/company/Project.java
@@ -1,16 +1,22 @@
 package com.company;
 
 //Ryan Xie and Josh Titens Final Project
-//Import Scanner
+
+//Link to our presentation: https://docs.google.com/presentation/d/1fjg22G8Ym1bDjzz8OkN3IZje31Y4G679amKEHGZaXHY/edit?usp=sharing
+
+//Import Scanner and File
 //create file
 //file needs to be 20 lines of 20 int, doubles, and words for each line (400) token total
 //if they say yes ask them whether they want to play the word guessing game or the number guessing game
-//If they want to play the numbers version activate the method that allows them guess numbers and words from the file
-//make this method scan through the file to see if the word/ number the user guessed is in the file
+//if they say yes ask them whether they want to play the word guessing game or the number guessing game
+// If they want to play the word version activate the method that allows them guess words from the file
+//make method scan through the file to see if the word the user guessed is in the file
+//If the answer the user gives is number and not word, repeat the same process but activate the numbers game (via else statement)
+//Regardless of number or word game, add score to scoreboard whenever user gets correct answer (if they get one correct they get 1/1 if they get one wrong they are 0/1 and keep track and add all of these idiviudal scores
+////per question and add them to make a final score at the end)
 //If they want to play the game again let them play again
-//if they get one correct they get 1/1 if they get one wrong they are 0/1 and keep track and add all of these idiviudal scores
-//per question and add them to make a final score at the end
 //when the user is finished playing calculate their score out of the times they guessed
+//Create two methods one for finding words in the file and one for finding numbers in file
 
 import java.util.*;
 import java.io.File;
