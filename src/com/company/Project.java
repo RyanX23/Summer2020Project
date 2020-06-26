a7ed66fd730e5202a34237776ae2bf842bafb407
Ryan Xie <xier23@greenhill.org>
2020-06-26 08:52:41 -0500

    Final Project

--- src/com/company/Project.java
+++ src/com/company/Project.java
@@ -1,4 +1,17 @@
 package com.company;
+
+//Ryan Xie and Josh Titens Final Project
+//Import Scanner
+//create file
+//file needs to be 20 lines of 20 int, doubles, and words for each line (400) token total
+//if they say yes ask them whether they want to play the word guessing game or the number guessing game
+//If they want to play the numbers version activate the method that allows them guess numbers and words from the file
+//make this method scan through the file to see if the word/ number the user guessed is in the file
+//If they want to play the game again let them play again
+//if they get one correct they get 1/1 if they get one wrong they are 0/1 and keep track and add all of these idiviudal scores
+//per question and add them to make a final score at the end
+//when the user is finished playing calculate their score out of the times they guessed
+
 import java.util.*;
 import java.io.File;
 import java.io.FileNotFoundException;
